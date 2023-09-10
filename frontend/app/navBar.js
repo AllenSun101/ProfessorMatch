8dfbf1b9ed5e1e41c9356bacc6d49e88b2d0dfdc
AllenSun101 <allensun21527@gmail.com>
2023-09-10 01:56:24 -0500

    fix

--- frontend/app/navBar.js
+++ frontend/app/navBar.js
@@ -5,12 +5,14 @@ export default function NavBar() {
     return (
         <nav className="bg-aggie flex justify-between px-10 items-center py-2" style={{ height: '90px' }}>
         <div className="flex items-center space-x-2">
+        <Link href={"/"}>
             <img
-            src="https://brandguide.tamu.edu/assets/img/logos/tam-primary-mark-a.png"
-            alt="My Image"
-            width={300}
-            height={300}
+                src="https://brandguide.tamu.edu/assets/img/logos/tam-primary-mark-a.png"
+                alt="My Image"
+                width={300}
+                height={300}
             />
+        </Link>
         </div>
         <div className="flex-grow"></div>
         <div className="font-bold text-white">
