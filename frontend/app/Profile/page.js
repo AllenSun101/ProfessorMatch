e81d27589193717b5c8d128ecbc0a39ebd33e23c
AllenSun101 <allensun21527@gmail.com>
2023-09-09 19:11:36 -0500

    Added flask

---
+++ frontend/app/Profile/page.js
@@ -0,0 +1,18 @@
+// get sqlite stuff
+
+export default function Profile(){
+    // retrieve
+    var feedback = "N/A";
+    var learnObjectives = "N/A";
+    var clearExpectations = "N/A";
+    var criticalThinking = "N/A";
+    var diverseIdeas = "N/A";
+    var courseOrganization = "N/A";
+    var gradeDistribution = "N/A";
+
+    return(
+        <div>
+
+        </div>
+    )
+}
---
+++ frontend/app/Search/page.js
@@ -0,0 +1,10 @@
+export default function Search(){
+
+    
+
+    return(
+        <div className="bg-white">
+            <h1></h1>
+        </div>
+    )
+}
