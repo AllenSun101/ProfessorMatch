10709091e39de6c8b22b98acdb0aaee6d134f68a
AllenSun101 <allensun21527@gmail.com>
2023-09-16 16:05:58 -0500

    Removed database backend, added visualizations page

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import sqlite3
 import analysis
 import pandas as pd
 
@@ -26,37 +25,6 @@ def temp_ratings(course, feedback, learn, expectations, critical, diverse, clear
 
     print(result_dict)
     return result_dict
-
-
-# Once preferences are stored in sql database 
-@app.route('/Fetch_Data')
-def get_ratings():
-    pass
-
-
-# Once login and sign up info are complete
-@app.route('/Input_Preferences')
-def input_references(preferences):
-    conn = sqlite3.connect('user_preferences.db')
-    cursor = conn.cursor()
-
-    cursor.execute('''CREATE TABLE IF NOT EXISTS users
-                   (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                    username VARCHAR(50) NOT NULL,
-                    password VARCHAR(50) NOT NULL,
-                    feedback INTEGER NOT NULL;
-                    objectives INTEGER NOT NULL;
-                    clear_expectations INTEGER NOT NULL;
-                    critical_thinking INTEGER NOT NULL;
-                    diversity INTEGER NOT NULL;
-                    organization INTEGER NOT NULL;
-                    grade_distribution DOUBLE NOT NULL;)
-                   ''')
-    
-    # Add user inputs
-
-    #cursor.execute('''INSERT INTO users (username, password, feedback, objectives, clear_expectations, critical_thinking, diversity, organization, grade_distribution) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
-    #               (stuff))
 
 
 if __name__ == '__main__':
---
+++ frontend/app/Trends/page.js
@@ -0,0 +1,7 @@
+export default function Trends(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
