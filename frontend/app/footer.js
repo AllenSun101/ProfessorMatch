c4f069233df2ed45acc5f40a1de838efdc084565
RandilW <85533296+RandilW@users.noreply.github.com>
2023-09-18 16:36:10 -0500

    added footer to layout.js

---
+++ frontend/app/footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <div className="bg-aggie text-cente lg:text-left">
+            <div className="p-4 text-center text-white">
+                © 2023 Copyright
+                <a
+                    className="text-neutral-100 dark:text-neutral-300"
+                    href="/"
+                > ProfMatch</a
+                >
+            </div>
+        </div>
+    )
+}
