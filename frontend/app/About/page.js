c19168fc42ba27be3169a622da0e4ce7a00de402
AllenSun101 <allensun21527@gmail.com>
2023-09-20 00:24:35 -0500

    Added about page

---
+++ frontend/app/About/page.js
@@ -0,0 +1,7 @@
+export default function About(){
+    return(
+        <div>
+            <p>Move about content here, add credits and contributors</p>
+        </div>
+    )
+}
--- frontend/app/navBar.js
+++ frontend/app/navBar.js
@@ -54,6 +54,7 @@ export default function NavBar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                     </button>
                 </Link>
+                <Link href="/About"> About </Link>
                 <Link href="/Trends"> Trends </Link>
                 <Link href="/Feedback"> Contact </Link>
             </div>
