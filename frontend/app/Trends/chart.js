83fca7403cf62a84bccae75f692cc5a23c68bc48
AllenSun101 <allensun21527@gmail.com>
2023-10-02 03:03:26 -0500

    Added checkboxes for Trends

--- frontend/app/Trends/chart.js
+++ frontend/app/Trends/chart.js
@@ -2,6 +2,7 @@
 
 import { Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import dynamic from 'next/dynamic';
+import { useState } from 'react';
 
 const LineChart = dynamic(() => (
     import("recharts").then(recharts => recharts.LineChart)
@@ -10,17 +11,34 @@ const LineChart = dynamic(() => (
 export default function Chart(props){
     // client side processing of chart component
 
+    const [selectedLines, setSelectedLines] = useState([]);
+
+    const handleCheckboxChange = (index) => {
+        if (selectedLines.includes(index)) {
+        setSelectedLines(selectedLines.filter((i) => i !== index));
+        } else {
+        setSelectedLines([...selectedLines, index]);
+        }
+    };
+
+    const handleSelectAll = () => {
+        const allIndices = Array.from({ length: profs.length }, (_, index) => index);
+        setSelectedLines(allIndices);
+    };
+    
+    const handleSelectNone = () => {
+        setSelectedLines([]);
+    };
+
     var profs = [];
     var semesterRatings = [];
 
 
     Object.entries(props.props).forEach(([prof, ratings]) => {
-        //console.log(`${prof}`);
 
         profs.push(prof);
 
         Object.entries(ratings).forEach(([semester, rating]) => {
-            //console.log(`${semester} : ${rating}`);
 
             const containsValue = semesterRatings.some(obj => obj.name === semester);
 
@@ -36,10 +54,8 @@ export default function Chart(props){
                 semesterRatings.push(sem);
             }
         });
-        //console.log(semesterRatings);
     });
 
-    //console.log(ratings);
 
     // sort semester
     const getSortableSemesterValue = (semester) => {
@@ -72,12 +88,33 @@ export default function Chart(props){
 
     return(
         <div>
+            <div className="grid grid-cols-3 gap-4 text-center">
+                <label className="mb-2">
+                    <input type="checkbox" onChange={handleSelectAll} checked={selectedLines.length === profs.length} className='checkbox '/>
+                    Select All
+                </label>
+                <label className="mb-2">
+                    <input type="checkbox" onChange={handleSelectNone} checked={selectedLines.length === 0} className='checkbox '/>
+                    Select None
+                </label>
+                {profs.map((element, index) => (
+                    <label key={index} className="mb-2">
+                        <input
+                        type="checkbox"
+                        checked={selectedLines.includes(index)}
+                        onChange={() => handleCheckboxChange(index)}
+                        className="mr-2 checkbox "
+                        />
+                        {element}
+                    </label>
+                ))}
+            </div>
             <LineChart width={1000} height={500} data={semesterRatings}
                 margin={{ top: 5, right: 60, left: 5, bottom: 80 }}>
                 <XAxis dataKey="name" angle={-45} dy={45}/>
                 <YAxis domain={[0, 5]} tickCount={11} ticks={[0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]}/>
                 <Tooltip />
-                {profs.map((element, index) => (
+                {profs.map((element, index) => (selectedLines.includes(index) && (
                     <Line
                         key={index}
                         type="monotone"
@@ -85,7 +122,7 @@ export default function Chart(props){
                         stroke={generateRandomColor()}
                         
                     />
-                ))}
+                )))}
             </LineChart>
         </div>
     )
