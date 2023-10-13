8d331cd0112ac092537a907d407e7e6ae9ae5046
RandilW <85533296+RandilW@users.noreply.github.com>
2023-10-13 18:41:52 -0500

    made navbar mobile friendly

--- frontend/app/navBar.js
+++ frontend/app/navBar.js
@@ -2,18 +2,21 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function NavBar() {
-    return (
 
+    return (
         <div className="navbar bg-aggie text-white">
-            <div className="navbar-start bg-aggie flex justify-between px-10 items-center py-2" style={{ height: '90px' }}>
+            <div className="navbar-start bg-aggie flex justify-between px-10 items-center py-2"
+                 style={{height: '90px'}}>
                 <div className="flex items-center space-x-2">
                     <Link href={"/"}>
-                        <Image
-                            src="https://brandguide.tamu.edu/assets/img/logos/tam-primary-mark-a.png"
-                            alt="My Image"
-                            width={300}
-                            height={300}
-                        />
+                        <div className="logo-image">
+                            <Image
+                                src="https://brandguide.tamu.edu/assets/img/logos/tam-primary-mark-a.png"
+                                alt="My Image"
+                                width={300}
+                                height={300}
+                            />
+                        </div>
                     </Link>
                 </div>
             </div>
@@ -24,14 +27,31 @@ export default function NavBar() {
                 </Link>
             </div>
 
-            <div className="navbar-end space-x-4">
+            <div className="navbar-end space-x-4 md:hidden">
+                <div class="dropdown dropdown-end">
+                    <label tabIndex="0" className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M4 6h16M4 12h16M4 18h7"/>
+                        </svg>
+                    </label>
+                    <ul tabindex="0"
+                        class="text-black menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-72 text-lg mt-4">
+                        <li><Link href="/About">About</Link></li>
+                        <li><Link href="/Search">Search</Link></li>
+                        <li><Link href="/Trends">Trends</Link></li>
+                        <li><Link href="/Feedback">Contact</Link></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="navbar-end space-x-4 hidden md:flex">
                 <Link className="px-4 py-2" href="/About">About</Link>
                 <Link className="px-4 py-2" href="/Search">Search</Link>
                 <Link className="px-4 py-2" href="/Trends">Trends</Link>
                 <Link className="px-4 py-2" href="/Feedback">Contact</Link>
             </div>
         </div>
-
-
     )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,31 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        'aggie': '#500000',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './node_modules/flowbite/**/*.js',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            colors: {
+                'aggie': '#500000',
+            },
+        },
     },
-  },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "light", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-  },
+    plugins: [require('daisyui', 'flowbite/plugin')],
+    daisyui: {
+        themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+        darkTheme: "light", // name of one of the included themes for dark mode
+        base: true, // applies background color and foreground color for root element by default
+        styled: true, // include daisyUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+        prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+        logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    },
 }
