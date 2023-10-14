07cb3e5077d73133bb42ee3e4f98730b57359b8d
AllenSun101 <allensun21527@gmail.com>
2023-10-14 12:59:56 -0500

    got rid of warnings

--- frontend/app/navBar.js
+++ frontend/app/navBar.js
@@ -28,16 +28,16 @@ export default function NavBar() {
             </div>
 
             <div className="navbar-end space-x-4 md:hidden">
-                <div class="dropdown dropdown-end">
+                <div className="dropdown dropdown-end">
                     <label tabIndex="0" className="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24"
                              stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                   d="M4 6h16M4 12h16M4 18h7"/>
                         </svg>
                     </label>
-                    <ul tabindex="0"
-                        class="text-black menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-72 text-lg mt-4">
+                    <ul tabIndex="0"
+                        className="text-black menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-72 text-lg mt-4">
                         <li><Link href="/About">About</Link></li>
                         <li><Link href="/Search">Search</Link></li>
                         <li><Link href="/Trends">Trends</Link></li>
